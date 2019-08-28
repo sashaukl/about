750f0004ab40fe70f26d46dc8e6c022f13992cb9
sashaukl <lexxoloff@gmail.com>
2019-08-28 19:19:49 +0300

    Updates

--- precache-manifest.bf2312ad83c59988f2638ee6994c9cae.js
+++ precache-manifest.bf2312ad83c59988f2638ee6994c9cae.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "e32fc3715e65d396a80dd458f400d2e5",
+    "revision": "1cbe8b6a7c65c52b577218e8a4b9d9bd",
     "url": "/github-router/index.html"
   },
   {
@@ -8,15 +8,15 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/github-router/static/css/2.266e55a5.chunk.css"
   },
   {
-    "revision": "4f62ad27e6bef534913e",
-    "url": "/github-router/static/css/main.207e9735.chunk.css"
+    "revision": "c0402e747741ccf0dd0e",
+    "url": "/github-router/static/css/main.46896d14.chunk.css"
   },
   {
     "revision": "1012095a8dbe161085d2",
     "url": "/github-router/static/js/2.4de6dfe5.chunk.js"
   },
   {
-    "revision": "4f62ad27e6bef534913e",
+    "revision": "c0402e747741ccf0dd0e",
     "url": "/github-router/static/js/main.dc00bde3.chunk.js"
   },
   {
