c8cb18ac4cc8b040666f410dcf6a3fd746eedad9
sashaukl <lexxoloff@gmail.com>
2019-08-29 20:11:58 +0300

    Updates

--- precache-manifest.75e29a54e36275f2fc0220897110c8a2.js
+++ precache-manifest.75e29a54e36275f2fc0220897110c8a2.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "b3f6e08fddc077debcbfc5f56a930ed5",
+    "revision": "5b2375f46130532642ca00f7bc8e04d1",
     "url": "/github-router/index.html"
   },
   {
-    "revision": "9cb10077b4ab75662ddf",
+    "revision": "f5c3ed06789a54620d12",
     "url": "/github-router/static/css/2.266e55a5.chunk.css"
   },
   {
-    "revision": "2af011f1554b11a96b50",
+    "revision": "265b2c509a9486f2a824",
     "url": "/github-router/static/css/main.a7425eba.chunk.css"
   },
   {
-    "revision": "9cb10077b4ab75662ddf",
-    "url": "/github-router/static/js/2.fb4d0ad8.chunk.js"
+    "revision": "f5c3ed06789a54620d12",
+    "url": "/github-router/static/js/2.9cb19a03.chunk.js"
   },
   {
-    "revision": "2af011f1554b11a96b50",
-    "url": "/github-router/static/js/main.63c6aff4.chunk.js"
+    "revision": "265b2c509a9486f2a824",
+    "url": "/github-router/static/js/main.3b23d2eb.chunk.js"
   },
   {
     "revision": "22a5fa18a217cf0f6e58",
