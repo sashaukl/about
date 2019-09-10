502c27c8d211b17b0f81c87c7abeb04520a4ff27
sashaukl <lexxoloff@gmail.com>
2019-09-11 01:59:34 +0300

    Updates

---
+++ precache-manifest.18b5fc11cc1591e4c2c2e169a39258ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3ae43f95c5ac15fdea0a8360cf3fb51",
+    "url": "/about/index.html"
+  },
+  {
+    "revision": "a10e1e038bc2efa4d804",
+    "url": "/about/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "b6ca34754992309e762d",
+    "url": "/about/static/css/main.8cd14a3e.chunk.css"
+  },
+  {
+    "revision": "a10e1e038bc2efa4d804",
+    "url": "/about/static/js/2.74c7b9de.chunk.js"
+  },
+  {
+    "revision": "b6ca34754992309e762d",
+    "url": "/about/static/js/main.5cac8782.chunk.js"
+  },
+  {
+    "revision": "5e7b65462749e51da51f",
+    "url": "/about/static/js/runtime~main.c0e869d3.js"
+  }
+]);
